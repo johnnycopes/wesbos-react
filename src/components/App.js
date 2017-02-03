4445209ff45289c14482d7ca3ec34092cf4210de
John Coppola <jdcoppola@gmail.com>
2017-02-02 22:19:19 -0500

    learning about state

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,28 @@ import Inventory from './Inventory';
 import NotFound from './NotFound';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.addFish = this.addFish.bind(this);
+    // getinitialstate -- setting the initial state
+    this.state = {
+      fishes: {},
+      order: {}
+    }
+  }
+
+  addFish(fish) {
+    // update state
+    // first, take a copy of the state
+    const fishes = {...this.state.fishes};
+    // add in our new fish, using a timestamp as an identifier
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+    // set state
+    this.setState({ fishes }); // ES6 syntax for fishes: fishes
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -12,7 +34,7 @@ class App extends React.Component {
           <Header tagline="Fresh Fish Market"/>
         </div>
         <Order/>
-        <Inventory/>
+        <Inventory addFish={this.addFish} />
       </div>
     )
   }
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,17 +2,40 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  // user-created methods don't bind 'this' to the React component, so you have to do it yourself. there are a couple of ways to do so:
+
+  // // 1) use the constructor of a component to add additional stuff during creation of the component
+  // constructor() {
+  //   super();
+  //   // bind the value of 'this' in the function to refer to the component
+  //   this.goToStore = this.goToStore.bind(this);
+  // }
+
+  goToStore(event) {
+    event.preventDefault();
+    // first, grab the text from the box -- we can do this now that we've bound the goToStore method to the component
+    const storeId = this.storeInput.value;
+    // second, we're going to transition from / to /store/:id
+    this.context.router.transitionTo(`/store/${storeId}`);
+  }
+
   // normal JS comments work fine outside of the HTML
   render() {
     return (
-      <form className="store-selector">
+      // 2) same idea, but bind 'this' on the fly
+      <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
         { /* This is a comment in JSX -- do NOT put it at the top level or it'll throw an error */ }
         <h2>Please enter a store</h2>
-        <input type="text" placeholder="Store Name" defaultValue={getFunName()} required/>
+        <input type="text" placeholder="Store Name" defaultValue={getFunName()} required ref={(input) => {this.storeInput = input} }/>
         <button type="submit">Visit Store →</button>
       </form>
     )
   }
+}
+
+// this tells React that StorePicker expects something called a router
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
 }
 
 export default StorePicker;
